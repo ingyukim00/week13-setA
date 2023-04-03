4f6b753a71ec48bc52e7cc1be04d2406f7491453
ingyukim <ikim59@bcit.ca>
2023-04-03 15:27:38 -0700

    Example 1 Responsive design

--- script.js
+++ script.js
@@ -1,38 +1,40 @@
-const buttonContainer = document.querySelector(".button-container");
-console.log(buttonContainer);
+// const buttonContainer = document.querySelector(".button-container");
+// console.log(buttonContainer);
 
-function changeBGGreen(event) {
-    if (event.target.tagName === "BUTTON") {
-        event, target.classList.add("greenBG");
-    };
-};
+// function changeBGGreen(event) {
+//     if (event.target.tagName === "BUTTON") {
+//         event, target.classList.add("greenBG");
+//     };
+// };
 
-buttonContainer.addEventListener("mouseover", changeBGGreen);
+// buttonContainer.addEventListener("mouseover", changeBGGreen);
 
 
-function changeTextColor(event) {
-    if (event.target.tagName === "BUTTON") {
-        let myColor = event.target.textContent;
-        event.target.style.color = myColor;
-        // event.target.classList.add(myColor);
-    };
-};
+// function changeTextColor(event) {
+//     if (event.target.tagName === "BUTTON") {
+//         let myColor = event.target.textContent;
+//         event.target.style.color = myColor;
+//         // event.target.classList.add(myColor);
+//     };
+// };
 
-buttonContainer.addEventListener("click", changeTextColor);
+// buttonContainer.addEventListener("click", changeTextColor);
 
-let buttonRef = document.querySelector("#clickMe");
+// let buttonRef = document.querySelector("#clickMe");
 
-function addNewbuttonAndPara() {
-    let newButton = document.createElement("button");
-    let newPara = document.createElement("p");
-    newButton.textContent = "purple";
-    // document.body.appendChild(newPara);
-    document.body.appendChild(newButton);
-    // newButton.addEventListener("click", changeBGGreen);
-    // newButton.addEventListener("click", changeTextColor);
-    // buttonContainer.appendChild(newButton);
+// function addNewbuttonAndPara() {
+//     let newButton = document.createElement("button");
+//     let newPara = document.createElement("p");
+//     newButton.textContent = "purple";
+//     // document.body.appendChild(newPara);
+//     document.body.appendChild(newButton);
+//     // newButton.addEventListener("click", changeBGGreen);
+//     // newButton.addEventListener("click", changeTextColor);
+//     // buttonContainer.appendChild(newButton);
 
-};
+// };
 
-buttonRef.addEventListener("click", addNewbuttonAndPara);
+// buttonRef.addEventListener("click", addNewbuttonAndPara);
+
+
 
